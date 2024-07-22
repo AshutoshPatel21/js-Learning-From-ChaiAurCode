d9f841b4b70faccbb78181feca4154b3cceec2f2
Ashutosh Patel <75129279+AshutoshPatel21@users.noreply.github.com>
2024-07-22 18:32:00 +0000

    learned about dataTypes in js

---
+++ 1.Basics/02.dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"  //treat all js code as newer version
+
+//alert(9+9) // we are using node.js not browser, we can use bt learn later
+
+//tc39.es site is official documentation of js
+let name= "ashutosh"
+let age= 25
+let isLoggedIn= false
+
+/* premittive dataTypes 
+
+number=> 2^53
+bigint => used for long value of number
+string => ""
+boolean => true/false
+null => standalone value(no value, object)
+undefined =>
+symbol => unique
+
+*/
+//non-premitive
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined)  //undefined
