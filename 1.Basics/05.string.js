4dd13230fedf77294dad470032262e753930f7a8
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-24 21:44:39 +0530

    learned more string methodes

--- 1.Basics/05.string.js
+++ 1.Basics/05.string.js
@@ -1,15 +1,33 @@
-const name ="ashutosh";
+// const name ="ashutosh";
 
-const myRepoCount= 3;
-
-
-console.log(name + " " + myRepoCount) //old way 
-
-console.log(`my name is ${name} and repoCont is ${myRepoCount}`)  //string inerpolation
+// const myRepoCount= 3;
 
 
-const fullName= new String("Ashutosh Patel")
+// console.log(name + " " + myRepoCount) //old way 
 
-console.log(fullName.length)
-console.log(fullName.toUpperCase())
+// console.log(`my name is ${name} and repoCont is ${myRepoCount}`)  //string inerpolation
 
+
+// const fullName= new String("Ashutosh Patel")
+
+// // console.log(fullName.length)
+// // console.log(fullName.toUpperCase())
+// console.log(fullName.charAt(2))
+// console.log(fullName.indexOf('h'))  //returns first found char
+
+// const newString= fullName.substring(0,4)
+// console.log(newString)
+
+// const anotherString= fullName.slice(-7,-9)
+// // if indexEnd comes before the indexStart in the string then output will be empty string
+// console.log(anotherString)
+
+// const newStringOne="  Ashutosh   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim()) //deletes whitespaces
+
+const url= "https://hitesh.com/hitesh%20-choudhary"
+console.log(url.replace("%20","-"))
+console.log(url.includes("sundar"))     //checks if string present
+
+console.log(url.split("-"))
