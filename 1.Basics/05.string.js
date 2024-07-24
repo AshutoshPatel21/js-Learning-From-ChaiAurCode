7ee9fc39d3f2fe2e3699915afd8fd04a8b6d481f
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-24 20:15:25 +0530

    leaned about string

---
+++ 1.Basics/05.string.js
@@ -0,0 +1,15 @@
+const name ="ashutosh";
+
+const myRepoCount= 3;
+
+
+console.log(name + " " + myRepoCount) //old way 
+
+console.log(`my name is ${name} and repoCont is ${myRepoCount}`)  //string inerpolation
+
+
+const fullName= new String("Ashutosh Patel")
+
+console.log(fullName.length)
+console.log(fullName.toUpperCase())
+
