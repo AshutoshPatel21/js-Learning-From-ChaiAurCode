e075fd57279b69eceace335fb04f0f643a692c9f
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-25 15:50:48 +0530

    have confusion in math.random()*(max-min+1)+min

---
+++ 1.Basics/06.numsAndMaths.js
@@ -0,0 +1,42 @@
+/*********************nums **********************************/
+
+// const score =4009
+
+// const balance= new Number(100.234532)
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision(6))
+
+// const rupay=1000000
+
+// console.log(rupay.toLocaleString('en-IN'))
+
+/*********************************MATHS *************************************/
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.5))
+// console.log(Math.min(1,6,3,2))
+// console.log(Math.max(1,6,3,2))
+
+// console.log(Math.random())
+
+// console.log(Math.floor(math.random()*10)+1)
+
+const min=10
+const max = 22
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
+// console.log(max-min)
+// console.log(Math.random())
+console.log(Math.floor(9.9))
+
+
+//when we multiply math.random()*10 it give us 0 to 9 mult 0.9*10=9
+//bt we do multiplu math.random()*11 it give us 0 to 10 why?? mult 0.9*11=9.9
+// Math.floor(9.9)=>9
