1631976f54da71470719ca3894507e548a55bf8b
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-25 13:53:23 +0530

    just revision

--- 1.Basics/05.string.js
+++ 1.Basics/05.string.js
@@ -8,7 +8,7 @@
 // console.log(`my name is ${name} and repoCont is ${myRepoCount}`)  //string inerpolation
 
 
-// const fullName= new String("Ashutosh Patel")
+ const fullName= new String("Ashutosh Patel")
 
 // // console.log(fullName.length)
 // // console.log(fullName.toUpperCase())
@@ -18,16 +18,16 @@
 // const newString= fullName.substring(0,4)
 // console.log(newString)
 
-// const anotherString= fullName.slice(-7,-9)
-// // if indexEnd comes before the indexStart in the string then output will be empty string
-// console.log(anotherString)
+const anotherString= fullName.slice(4,0)
+// if indexEnd comes before the indexStart in the string then output will be empty string
+console.log(anotherString)
 
 // const newStringOne="  Ashutosh   "
 // console.log(newStringOne)
 // console.log(newStringOne.trim()) //deletes whitespaces
 
-const url= "https://hitesh.com/hitesh%20-choudhary"
-console.log(url.replace("%20","-"))
-console.log(url.includes("sundar"))     //checks if string present
+// const url= "https://hitesh.com/hitesh%20-choudhary"
+// console.log(url.replace("%20","-"))
+// console.log(url.includes("sundar"))     //checks if string present
 
-console.log(url.split("-"))
+// console.log(url.split("-"))
