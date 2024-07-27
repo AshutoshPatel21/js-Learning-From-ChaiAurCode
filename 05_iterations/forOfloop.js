05160551bca26abcf11d7b3e6ca52175ba67fba4
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-27 20:45:05 +0530

    learned for of loop

---
+++ 05_iterations/forOfloop.js
@@ -0,0 +1,35 @@
+//for of
+
+// const arr=[4,5,6,7]
+
+// for(let num of arr){
+//     // console.log(num)
+//     console.log(num)
+// }
+
+const myMap= new Map()
+
+myMap.set('key1',"ashutosh")
+myMap.set("key2", "patel")
+
+// for(let key of myMap){
+//     console.log(key)
+//  }
+
+// for(let [key,value] of myMap){
+//     // console.log(value)
+//     console.log(key)
+// }
+
+// console.log(myMap)
+
+// const myObj={
+//     name1:"ashutosh",
+//     name2:"patel"
+// }
+
+// cannot apply for of on object
+
+// for(let objValue of myObj){
+//     console.log(objValue)
+// }
