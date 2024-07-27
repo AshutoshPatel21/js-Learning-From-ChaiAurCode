99dcb3430fab9eb5300b54ebdddd8252b4b2cc65
Ashutosh Patel <ashutoshpatel021@gmail.com>
2024-07-27 22:33:41 +0530

    learned for in loop

---
+++ 05_iterations/forInLoop.js
@@ -0,0 +1,31 @@
+const myMap=(
+    'key', "ashutosh"
+
+)
+
+// console.log(myMap)
+
+const myObject = {
+    js:'javascript',
+    cpp:'c++',
+    rb: 'ruby',
+    seift: "swift by apple"
+}
+
+// for(const key in myObject){
+//     console.log(key)
+//     console.log(myObject[key])
+// }
+
+ const arr=["js","rb","swift"];
+// for(const i in arr){
+//     // console.log(i)
+//     console.log(arr[i])
+// }
+
+for (const key in myMap){
+    console.log(key)
+}
+
+
+//for in doesnt work for map like for of doesnt work for object
